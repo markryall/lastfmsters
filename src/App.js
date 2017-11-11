6edf791a449e92dfbdaf205b67c8cfa7c5de50e5
Mark Ryall <mark@ryall.name>
2017-11-11 21:40:56 +1100

    add a few simple commands

--- src/App.js
+++ src/App.js
@@ -11,32 +11,58 @@ class App extends Component {
         this.onChange = (event) => {
             this.setState({command: event.target.value});
         }
-        this.handleKeyPress = (event) => {
-            if(event.key === 'Enter'){
-                const history = this.state.history.slice(0);
-                history.push({
-                    command: `> ${this.state.command}`,
-                    response: this.state.command
-                });
-                this.setState({
-                    history: history,
+        const commandResponse = ( command ) => {
+            switch( command ) {
+                case "help":
+                    return ["help", "ls"];
+                case "ls":
+                    return [ "README.md    node_modules package.json public       src" ];
+                default:
+                    return [ `Unknown command "${command}"` ];
+            }
+        }
+        const newHistory = ( previousHistory, command ) => {
+            if ( command === 'clear' ) return [];
+            const history = previousHistory.slice(0);
+            history.push( `> ${command}` );
+            commandResponse( command ).forEach( ( line ) => { history.push( line ); } );
+            return history;
+        }
+        this.handleKeyPress = ( event ) => {
+            if ( event.key === 'Enter' ){
+                const { history, command } = this.state;
+                this.setState( {
+                    history: newHistory( history, command ),
                     command: ''
-                });
+                } );
             }
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.nameInput.focus();
+        /* axios.get(
+         *     "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&nowplaying=true&user=mryall&api_key=21f8c75ad38637220b20a03ad61219a4&format=json&limit=50&extended=1"
+         * ).then(
+         *     (result) => {
+         *         result.data.recenttracks.track.map(
+         *             (track) => {
+         *                 console.log(track.name)
+         *                 track.image.map(
+         *                     (image) => {
+         *                         console.log(`  ${image['#text']} (${image.size})`)
+         *                     }
+         *                 );
+         *             }
+         *         );
+         *     }
+         * );*/
     }
 
     render() {
-        const history = this.state.history.map((entry, index) => {
+        const history = this.state.history.map( ( entry ) => {
             return (
-                <div>
-                    <div>{ entry.command }</div>
-                    <div>{ entry.response }</div>
-                </div>
+                <div>{ entry }</div>
             );
         });
     return (
@@ -52,6 +78,7 @@ class App extends Component {
             <input
                 id="input"
                 ref={(input) => { this.nameInput = input; }} 
+                onKeyUp={this.handleKeyUp}
                 onKeyPress={this.handleKeyPress}
                 onChange={this.onChange}
                 value={ this.state.command }
